37bfb15837753e3f8b1989b140f61c0ac3e3b6f2
nam1812hy <nam1812hy@gmail.com>
2023-08-25 17:22:38 +0700

    ud

--- FRONTEND/src/components/core/form/index.tsx
+++ FRONTEND/src/components/core/form/index.tsx
@@ -2,6 +2,7 @@ import type { ControlTypes, MinvoiceFormItemProps } from '../form-item';
 import type { FormProps } from 'antd/es/form/Form';
 import { Form } from 'antd';
 import MinvoiceformItem from '../form-item';
+import React from 'react';
 
 export interface MyFormOptions extends Array<MinvoiceFormItemProps<ControlTypes>> { }
 export interface MyFormProps<T> extends FormProps<T> {
@@ -11,11 +12,12 @@ export interface MyFormProps<T> extends FormProps<T> {
 const BaseForm = <Values extends object>(props: MyFormProps<Values>) => {
   const { fieldOptions, children, ...rest } = props;
 
+  /**React.Children.toArray tự động gán keys duy nhất cho các phần tử con */
   return (
     <Form<Values> {...rest}>
-      {fieldOptions?.map(option => {
+      {React.Children.toArray(fieldOptions?.map(option => {
         return <MinvoiceformItem {...option} />;
-      })}
+      }))}
       {children}
     </Form>
   );
